f1a175675c4b14a0c96b2ab7a04c2b23cbff9388
7reyor <7reyor@gmail.com>
2016-03-02 20:16:48 -0800

    Added description of program

--- src/helloWorld.scala
+++ src/helloWorld.scala
@@ -1,11 +1,12 @@
 /**
   * Created by 7reyor on 3/2/2016.
+  * This is a simple Hello World program.
   */
 object helloWorld {
 
   def main(args: Array[String]) {
 
-    println("Hello, world!");
+    println("Hello, world!")
 
   }
 
