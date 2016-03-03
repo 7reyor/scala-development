be34b36a8a9be3c466583edd447a193e28e6edff
7reyor <7reyor@gmail.com>
2016-03-02 20:21:03 -0800

    Updated description.

--- src/helloWorld.scala
+++ src/helloWorld.scala
@@ -1,6 +1,7 @@
 /**
   * Created by 7reyor on 3/2/2016.
-  * This is a simple Hello World program.
+  *
+  * Description: outputs the text "Hello, world!" to the console. That's about it.
   */
 object helloWorld {
 
